d9cafeada70d03f404125f34b88a38cf2fa96354
Jacob Snodgress <jacobsnodgress5@gmail.com>
2025-07-22 10:29:21 -0700

    Add files via upload

---
+++ links.js
@@ -0,0 +1,7 @@
+document.getElementById("contact").addEventListener("click", function () {
+    window.location.href = "contact.html";
+});
+
+document.getElementById("contact1").addEventListener("click", function () {
+    window.location.href = "contact.html";
+});
