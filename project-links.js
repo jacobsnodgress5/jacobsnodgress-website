058b3feb9b136fc2dc46d50b7b0f6fccc6453242
Jacob Snodgress <jacobsnodgress5@gmail.com>
2026-02-22 18:57:25 -0800

    Add event listener for projects link redirection

--- project-links.js
+++ project-links.js
@@ -1,3 +1,6 @@
 document.getElementById("overlay").addEventListener("click", function () {
     window.location.href = "https://github.com/jacobsnodgress5/Neural-Network-HAPI-Project/blob/main/Neural-Network-HAPI%20(4).ipynb";
 });
+document.getElementById("projects").addEventListener("click", function () {
+    window.location.href = "UFA.html";
+});
